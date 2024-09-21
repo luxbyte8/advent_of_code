831a6fdb56104c9e7280bb8df1568ab78b6c1ee3
luxbyte8 <luxbyte@hotmail.com>
2024-09-21 17:16:16 -0400

    finish 2015/day4 the hard way; implemented md5 hash algorithm

--- 2015/day4/day4.cpp
+++ 2015/day4/day4.cpp
@@ -1,55 +1,97 @@
 #include <cmath>
 #include <cstdint>
 #include <cstdlib>
+#include <fstream>
 #include <iomanip>
 #include <iostream>
+#include <sstream>
 #include <string>
 #include <vector>
 
+#include "md5.h"
+
 using namespace std;
 
-// All variables are unsinged 32-bit and mod 2^32
-
-// per-round shift amounts
-const vector<uint32_t> MD5_SHIFT {
-    7, 12, 17, 22, // Round 1
-    5, 9,  14, 20, // Round 2
-    4, 11, 16, 23, // Round 3
-    6, 10, 15, 21, // Round 4
-    7, 12, 17, 22, // Round 5
-    5, 9,  14, 20, // Round 6
-    4, 11, 16, 23, // Round 7
-    6, 10, 15, 21, // Round 8
-    7, 12, 17, 22, // Round 9
-    5, 9,  14, 20, // Round 10
-    4, 11, 16, 23, // Round 11
-    6, 10, 15, 21, // Round 12
-    7, 12, 17, 22, // Round 13
-    5, 9,  14, 20, // Round 14
-    4, 11, 16, 23, // Round 15
-    6, 10, 15, 21  // Round 16
-};
-
-vector<uint32_t> compute_md5_k();
-const vector<uint32_t> MD5_K = compute_md5_k();
-
-uint32_t A = 0x67452301;
-uint32_t B = 0xefcdab89;
-uint32_t C = 0x98badcfe;
-uint32_t D = 0x10325476;
+void test(const string&);
+void solution(const string&);
 
 int main()
 {
-    for(const auto &x : MD5_K) {
-        cout << hex << x << " " << endl;
-    }
+    test("input.txt");
+    solution("input.txt");
+    solution("puzzle.txt");
     return EXIT_SUCCESS;
+} // main
+
+void test(const string &filename)
+{
+    MD5 md5{};
+    fstream ifs(filename, ios::in | ios::binary);
+    // how it could be used:
+    // by reading chunks of data or you could read
+    // entire data and call update and finalize once
+    if(ifs.is_open()) {
+        char buffer[512];
+        // grab a chunk; in this case chunk of 512 bytes
+        while(ifs.read(buffer, sizeof(buffer))) {
+            md5.update(reinterpret_cast<uint8_t*>(buffer), ifs.gcount());
+        }
+        // any leftover data
+        if(ifs.gcount() > 0) {
+            md5.update(reinterpret_cast<uint8_t*>(buffer), ifs.gcount());
+        }
+        vector<uint8_t> digest;
+        md5.finalize(digest);
+        cout << "test: md5: 0x";
+        for(uint8_t byte : digest)
+            printf("%02x", byte);
+        printf("\n");
+    }
 }
 
-vector<uint32_t> compute_md5_k()
+void solution(const string &filename)
 {
-    vector<uint32_t> md5_k(64);
-    for(size_t i = 0; i < 64; ++i)
-        md5_k[i] = static_cast<uint32_t>(floor((1ULL << 32) * abs(sin(i + 1))));
-    return md5_k;
+    fstream ifs(filename, ios::in | ios::binary);
+    if(!ifs.is_open()) {
+        cerr << "unable to open file: " << filename << endl;
+        exit(1);
+    }
+    char c;
+    string msg;
+    while(ifs.get(c)) {
+        msg += c;
+    }
+    size_t num = 0;
+    stringstream ss(msg); 
+    vector<uint8_t> digest;
+    bool once = true;
+    while(true) {
+        MD5 md5{};
+        digest.clear();
+        md5.update(reinterpret_cast<const uint8_t*>(ss.str().data()), ss.str().size());
+        md5.finalize(digest);
+        // part 1: starts with 5 zeros
+        if(digest[0] == 0x00 && digest[1] == 0x00 && (digest[2] >> 4) == 0x00) {
+            if(once) {
+                cout << "part 1: " << ss.str() << ", md5: 0x";
+                for(uint8_t byte : digest)
+                    printf("%02x", byte);
+                printf("\n");
+            }
+            once = false;
+            // part 2: starts with 6 zeros
+            if(digest[0] == 0x00 && digest[1] == 0x00 && digest[2] == 0x00) {
+                cout << "part 2: " << ss.str() << ", md5: 0x";
+                for(uint8_t byte : digest)
+                    printf("%02x", byte);
+                printf("\n");
+                break;
+            }
+        } 
+        ss.str(""); // reset content
+        ss.clear(); // reset flags
+        ss << msg << num;
+        num++;
+    }
 }
+// EOF
